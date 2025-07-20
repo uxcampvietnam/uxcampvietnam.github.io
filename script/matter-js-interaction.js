10e05150375377bccb43c44708698712cad886f1
uxcampvietnam <214609648+uxcampvietnam@users.noreply.github.com>
2025-07-20 09:55:32 +0700

    update

--- script/matter-js-interaction.js
+++ script/matter-js-interaction.js
@@ -19,7 +19,7 @@ window.onload = function () {
     const height = canvas.height;
 
     const engine = Engine.create();
-    engine.world.gravity.y = 0.5;
+    engine.world.gravity.y = -0.2;
     engine.world.gravity.x = 0;
 
     const world = engine.world;
@@ -132,28 +132,26 @@ window.onload = function () {
         'asset/image/interactive-img/90.png',
         'asset/image/interactive-img/91.png',
         'asset/image/interactive-img/92.png',
-        'asset/image/interactive-img/93.png',
-        'asset/image/interactive-img/94.png',
-        'asset/image/interactive-img/95.png',
-        'asset/image/interactive-img/96.png',
-        'asset/image/interactive-img/97.png',
-        'asset/image/interactive-img/98.png',
-        'asset/image/interactive-img/99.png',
-        'asset/image/interactive-img/100.png',
-        'asset/image/interactive-img/101.png',
-        'asset/image/interactive-img/102.png',
-        'asset/image/interactive-img/103.png',
-        'asset/image/interactive-img/104.png',
-        'asset/image/interactive-img/105.png',
-        'asset/image/interactive-img/106.png',
-        'asset/image/interactive-img/107.png',
-        'asset/image/interactive-img/108.png'
+        'asset/image/interactive-img/93.png'
+        // 'asset/image/interactive-img/94.png',
+        // 'asset/image/interactive-img/95.png',
+        // 'asset/image/interactive-img/96.png',
+        // 'asset/image/interactive-img/97.png',
+        // 'asset/image/interactive-img/98.png',
+        // 'asset/image/interactive-img/99.png',
+        // 'asset/image/interactive-img/100.png',
+        // 'asset/image/interactive-img/101.png',
+        // 'asset/image/interactive-img/102.png',
+        // 'asset/image/interactive-img/103.png',
+        // 'asset/image/interactive-img/104.png',
+        // 'asset/image/interactive-img/105.png',
+        // 'asset/image/interactive-img/106.png',
+        // 'asset/image/interactive-img/107.png',
+        // 'asset/image/interactive-img/108.png'
     ];
 
-    var smallestWindowLength = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth;
-
-    const imageWidth = smallestWindowLength / 11.5; // pixels
-    const imageHeight = smallestWindowLength / 11.5;
+    const imageWidth = Math.round(Math.sqrt((canvas.width * canvas.height)/(imageUrls.length*1.8))); // pixels
+    const imageHeight = Math.round(Math.sqrt((canvas.width * canvas.height)/(imageUrls.length*1.8)));
     const bodies = [];
 
     function isAreaFree(x, y, w, h) {
@@ -184,7 +182,7 @@ window.onload = function () {
     let tries = 0;
     while (bodies.length < imageUrls.length && tries < 1000) {
         const x = width / 2;
-        const y = height / 4;
+        const y = height / 4 * 3;
         if (isAreaFree(x, y, imageWidth, imageHeight)) {
             const body = createImageBody(x, y, imageUrls[bodies.length]);
             bodies.push(body);
@@ -240,16 +238,32 @@ window.onload = function () {
     World.add(world, mouseConstraint);
     render.mouse = mouse;
 
-    // window.addEventListener('mousedown', () => {
-    //     const { x, y } = mouse.position;
-    //     if (isAreaFree(x, y, imageWidth, imageHeight)) {
-    //         const img = imageUrls[Math.floor(Math.random() * imageUrls.length)];
-    //         const body = createImageBody(x, y, img);
-    //         World.add(world, body);
-    //     }
-    // });
+    window.addEventListener('mousedown', () => {
+        const { x, y } = mouse.position;
+        if (isAreaFree(x, y, imageWidth, imageHeight)) {
+            const img = imageUrls[Math.floor(Math.random() * imageUrls.length)];
+            const body = createImageBody(x, y, img);
+            World.add(world, body);
+        }
+    });
 
-    window.addEventListener('resize', () => location.reload());
+    let resizeTimeout;
+    window.addEventListener('resize', () => {
+        // Clear any existing timeout to reset the timer
+        clearTimeout(resizeTimeout);
+
+        // Set a new timeout
+        resizeTimeout = setTimeout(() => {
+            // This function will execute only after the user stops resizing for 200ms
+            handleResizeEnd();
+        }, 200); // Adjust the delay (in milliseconds) as needed
+    });
+
+
+    function handleResizeEnd() {
+        window.innerWidth <= canvas.width ? console.log("cho nhỏ đi") : location.reload();;
+    }
+
 };
 
 
